ed1c7d13ecb051e76599a5b53aaa725a8eb51e69
Jeffrey Wills <jwills@rise8.us>
2021-03-18 19:38:08 -0400

    clean up

--- src/main/java/us/rise8/tracker/api/init/InitController.java
+++ src/main/java/us/rise8/tracker/api/init/InitController.java
@@ -5,14 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import us.rise8.tracker.api.init.dto.InfoDTO;
-import us.rise8.tracker.api.user.User;
-import us.rise8.tracker.api.user.UserService;
-import us.rise8.tracker.api.user.dto.UserDTO;
 import us.rise8.tracker.config.CustomProperty;
 
 @RestController
@@ -21,25 +17,17 @@ import us.rise8.tracker.config.CustomProperty;
 public class InitController {
 
     private final CustomProperty property;
-    private final UserService userService;
 
     @Autowired
-    public InitController(CustomProperty property, UserService userService) {
-        this.userService = userService;
+    public InitController(CustomProperty property) {
         this.property = property;
     }
 
-    @ApiOperation(value = "context info",
+    @ApiOperation(value = "Classification context info",
             notes = "Returns classification context, as well as available roles a user may have")
     @GetMapping("/info")
     public InfoDTO getInfo() {
         return new InfoDTO(property.getClassification(), property.getCaveat());
     }
 
-    @ApiOperation(value = "Returns currently logged in user",
-            notes = "PlatformOne identity management never informs the frontend of the user identity")
-    @GetMapping("/user")
-    public UserDTO loginInit() throws JsonProcessingException {
-        return new User().toDto();
-    }
 }
